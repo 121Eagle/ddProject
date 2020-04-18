be0d9cc4f95bcf723d72ed371c773b0feb0a3771
William Mironchuk <wmironchuk@umass.edu>
2020-04-18 21:40:50 +0000

    Did some work, mostly pointless, but messed arround with some argument values

---
+++ dd.py
@@ -0,0 +1,5 @@
+import glob
+import sys
+def dd(inputName, outputName='', moveSize=512, count=None, seek=0, skip=0):
+    with open(inputName,mode='rb') as fIn:
+        pass
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ if __name__ == "__main__": # don't remove this line
     p = argparse.ArgumentParser() # don't remove this line either
 
     ### EDIT THESE AS NEEDED ###
-    p.add_argument("file", type=str, help="name/path of the input file")
+    p.add_argument("if=", type=str, help="name/path of the input file")
     p.add_argument("-x", action="store_true", help="extract something True/False")
     p.add_argument("-n", action="store", help="specify some number")
     ############################
@@ -20,10 +20,3 @@ if __name__ == "__main__": # don't remove this line
             raise Exception("Given path is not a file!")
     else:
         raise Exception("Given path does not exist!")
-
-    if args.x:
-        print("Extract option specified.")
-        if args.n:
-            print("n specified; n = %s" % args.n)
-        else:
-            print("n not specified.")
