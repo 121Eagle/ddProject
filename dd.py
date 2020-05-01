5e04d4465b3c019e6646800d215d8c94fe813dff
William Mironchuk <wmironchuk@umass.edu>
2020-05-01 09:53:09 -0400

    I implemented most of the project so far, but I didn't add some command line options, and I implemented sparce files wrong Also it would be cool to try and use that thing that reduces switches between kernel space and userspace

--- dd.py
+++ dd.py
@@ -1,21 +1,51 @@
-import glob
-import os.path
 import math
-
 def sizeParse(N):
-    multiples = {'c':1,'w':2,'b':512,'KB':1000,'K':1024}
-    number,multiple = None
-    if(not math.isnan(N)):
+    multiples = {'c':1,'w':2,'b':512,'K':1024, 'KB':1000, 'M':1024 ** 2, 'G':1024 ** 3, 'T':1024 ** 4, 'P':1024 ** 5, 'E':1024 ** 6, 'Z':1024 ** 7, 'Y':1024 ** 8,
+                 'MB':1000 ** 2, 'GB':1000 ** 3, 'TB':1000 ** 4, 'PB':1000 ** 5, 'EB':1000 ** 6, 'ZB':1000 ** 7, 'YB':1000 ** 8,}
+    multiples.update({'x'+x:multiples[x] for x in multiples.keys()})
+    try:
+        N=int(N)
+    except ValueError:
+        j = list(multiples.keys())
+        j.sort(key=len(),reverse=True)
+        for i in j:
+            if i in N:
+                # This system will multiply the integer value, by the metric function given.
+                return (int(N[:-len(i)]) * multiple[i])
+        raise ValueError
+    except TypeError:
         return N
-    else if (not math.isnan(N[-2])):
-        number,multiple = int(N[:-1]),N[-1]
     else:
-        number,multiple = int(N[:-2]),N[-2:]
-    return number * multiples[multiple]
+        return N
+def sparce(data, blocksize):
+    if ((lindex := data.find(b'\x00')) > -1):
+        data = data[:lindex] + data[lindex:].strip(b'\x00')
+    if ((rindex := data.rfind(b'\x00')) > -1):
+        data = data[:rindex].rstrip(b'\x00') + data[rindex:]
+    length = len(data)
+    data = data + in_file.read(size - length)
+    return sparce(data,blocksize)
 
-def dd(inputName='', outputName='', blockSize=512, count=math.inf, seek=0, skip=0):
+def is_not_number(N):
+    return (not isinstance(N, (type(1),type(math.inf),type(0.1),type(math.pi))))
+
+def dd(inputName='', outputName='', blockSize=512, count=math.inf, seek='0', skip='0', conv=None):
+    size = sizeParse(blockSize)
+    if is_not_number(count):
+        count = sizeParse(count)
     with open(inputName,'rb') as in_file:
-        in_file.seek(sizeParse(skip)*sizeParse(blockSize),0)
+        in_file.seek(sizeParse(skip)*size,0)
         with open(outputName,'wb') as out_file:
-            out_file.seek(seek,0)
-            pass
+            out_file.seek(sizeParse(seek)*size,0)
+            while 0 < count :
+                data = in_file.read(size)
+                if (conv == 'sparse'):
+                    data = sparce(data,size)
+                if (conv == 'lcase'):
+                    data = data.lower()
+                else if (conv == 'ucase'):
+                    data = data.upper()
+                else:
+                    pass
+                out_file.write(in_file.read(size))
+                count = count - 1
--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 import argparse # don't remove me
 from os import path
+from dd import dd
 
 if __name__ == "__main__": # don't remove this line
 
     p = argparse.ArgumentParser() # don't remove this line either
 
     ### EDIT THESE AS NEEDED ###
-    p.add_argument("if=", type=str, help="name/path of the input file")
-    p.add_argument("-x", action="store_true", help="extract something True/False")
-    p.add_argument("-n", action="store", help="specify some number")
+    p.add_argument("input", type=str, action="store", metavar='if=', help="read from file")
+    p.add_argument("output", type=str, action="store", metavar='of=', help="write to file")
+    p.add_argument("blocksize", type=str, nargs='?', action="store", metavar='bs=', help="blocksize", default='bs=512')
+    p.add_argument("count", type=int, required=False, nargs='?', action='store', metavar='count=')
+    p.add_argument('conv', type=str, action='store', metavar='conv=', choices=('ucase','lcase','sparce'))
     ############################
 
     args = p.parse_args()
 
     ### YOUR LOGIC BELOW ###
+    variables = vars(args)
+    variables.update({x:variables[x].split('=')[1] for x in variables.keys()})
+    print(variables)
 
-    if path.exists(args.file):
-        if not path.isfile(args.file):
+    if (path.exists(args.input) and path.exists(args.output)):
+        if (not path.isfile(args.input)) and (not path.isfile(args.input)):
             raise Exception("Given path is not a file!")
     else:
         raise Exception("Given path does not exist!")
+    dd(**variables)
